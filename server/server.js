c118924d2d5471a4cc805aafd4d41c1c4b8da70f
VLAD-TREX <vladlenkotov0@gmail.com>
2026-01-16 16:43:40 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Mock-данные (30 игр)
 const mockGames = [
